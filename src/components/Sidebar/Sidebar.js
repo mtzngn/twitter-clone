ea8dc1ada4e312c7070d3a5595a9d2113f67a40f
mtzngn <mt.zngn@gmail.com>
2021-01-19 21:09:42 +0000

    sidebar halfway

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,32 @@
 import './Sidebar.css';
-import {React, useState} from "react"
+import {React, useState, useEffect} from "react"
 
 const Sidebar = () => {
+    const [data, setData] = useState(["Home", "Explore", "Notifications", "Messages", "Bookmarks", "Lists", "Profile", "More"])
+
+    useEffect(()=>{
+
+    },[])
     return (
     <div className="sidebar">
-      <h1>This is side bar</h1>
+      <ul>
+        <li>
+          <a>Logo</a>
+        </li>
+        {data.map((data)=>{
+          return(
+            <li>
+              <a href="#">
+                <i>Icon</i>
+                <h2>{data}</h2>
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <div className="user-section">
+
+      </div>
       </div>
     )
     }

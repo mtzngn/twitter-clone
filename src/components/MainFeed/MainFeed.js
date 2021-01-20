1fe74947ca87751644deb7701a22b6aa0ea3d28d
mtzngn <mt.zngn@gmail.com>
2021-01-20 18:41:25 +0000

    main feed component style and functionality added

--- src/components/MainFeed/MainFeed.js
+++ src/components/MainFeed/MainFeed.js
@@ -11,16 +11,42 @@ const MainFeed = () =>{
     return (
     <div class="main-feed">
       <UserInput getTweets={getTweets}/>
-      <div className="feed-wrapper">
-        <div className="feed-logo">
-          
-        </div>
-        <div className="feed-details">
-        </div>
-      </div>
+
       {tweets.map((tweet)=>{
         return (
-          <h1>{tweet}</h1>
+          <div className="feed-wrapper">
+          <div className="feed-logo">
+            
+          </div>
+          <div className="feed-details">
+            <div className="feed-title-section">
+              <h4>Chuck Norris</h4>
+              <p>@ChuckChuck</p>
+              <p> . </p>
+              <p>20 Jan</p>
+            </div>
+            <div className="tweet-feed">
+              <p>{tweet}</p>
+            </div>
+            <div className="tweet-btns">
+              <a href="#">
+                <i class="far fa-comment fa-lg"></i>
+              </a>
+              <a href="#">
+                <i class="fas fa-retweet fa-lg"></i>
+              </a>
+              <a href="#">
+                <i class="far fa-heart fa-lg"></i>
+              </a>
+              <a href="#">
+                <i class="far fa-share-square fa-lg"></i>
+              </a>
+  
+            </div>
+          </div>
+        </div>
+
+
         )
       })}
     </div>
